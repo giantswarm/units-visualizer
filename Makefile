8a8eb8b9e79b7388889141a2ed924138246c6d50
Oliver Ponder <oliver.ponder@gmail.com>
2016-05-04 12:18:42 +0200

    finetuning

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 build-and-run: build run
 
 run:
-	docker run -ti -p 8000:8000 oponder/unit-visualizer
+	docker run -ti -p 8000:8000 -v ${PWD}/public:/usr/src/app/public oponder/unit-visualizer
 
 build:
 	docker build -t oponder/unit-visualizer .
 
+simulate-up:
+	echo "POST http://docker.dev:8000/message" | vegeta attack -duration=5s -body=json_bodies/add1.json -rate=50 -header="Content-type:application/json" -output=/dev/null
+
+simulate-down:
+	echo "POST http://docker.dev:8000/message" | vegeta attack -duration=5s -body=json_bodies/subtract1.json -rate=50 -header="Content-type:application/json" -output=/dev/null
+
+simulate-reset:
+	echo "POST http://docker.dev:8000/message" | vegeta attack -duration=1s -body=json_bodies/setTo0.json -rate=200 -header="Content-type:application/json" -output=/dev/null
--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const bodyParser = require('body-parser');
 const morgan     = require('morgan'); // Request Logging to STDOUT
 const expressWs = require('express-ws');
 
-const PORT = 8000
+const PORT = 8000;
+var totalUnits = 0;
 
 var app = express();
+var messageCallback = function() {}; // No-op until someone connects to the websocket
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 expressWs(app);
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.send("Hello World!");
+});
+
+// Websocket endpoint that sends 'Hello'
+app.ws('/stream', function(ws, req) {
+  console.log("A browser connected")
+  ws.send(totalUnits.toString());
+
+  messageCallback = function(command){
+    if (command.setTotal) {
+      totalUnits = parseInt(command.setTotal);
+    }
+
+    if (command.increment) {
+      totalUnits += parseInt(command.increment);
+    }
+
+    ws.send(totalUnits.toString(), function(error) {});
+  }
+});
+
+app.post('/message', function (req, res) {
+  res.sendStatus(200);
+  messageCallback(req.body);
 });
 
 app.listen(8000, function () {
